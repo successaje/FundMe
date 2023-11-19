35d0230ca85450557868f7bc73dfe68a7887c6bc
Success Aje <successaje7@gmail.com>
2023-11-19 19:52:45 +0100

    changes

--- programs/fundme/src/lib.rs
+++ programs/fundme/src/lib.rs
@@ -9,10 +9,28 @@ declare_id!("49wKh7f9yc7qtch5upj52EA4dUqKNSVFb2Pm7SMVUbFH");
 pub mod fundme {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, name : String, age : String) -> Result<()> {
+
+        let profile = &mut ctx.accounts.profile;
+        profile.authority = ctx.accounts.authority.key();
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+
+    #[account(mut)]
+    pub authority : Signer<'info>,
+
+    #[account(
+        init, 
+        seeds = [authority.key().as_ref()],
+        bump,
+        payer = authority,
+        space = 8+ std::mem::size_of::<UserProfile>(),
+    )]
+
+    // pub userProfile
+}
